4c1bf5129166b1f6e60e70eec1dad5f9ddfdc6ee
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2023-08-17 01:27:00 +0530

    Update CTA.jsx

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -10,7 +10,7 @@ const CTA = () => {
         <div className="mt-4 md:mt-0">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Innovative and Powerful Solutions for a Digital World.</h2>
             <p className="mb-6  text-black md:text-lg dark:text-gray-400">Our team of experts leverages cutting-edge technologies such as React.js, Node.js, MongoDB, Express, and WordPress to ensure that every project is completed on-time and to the highest standards. With a focus on meeting and exceeding client expectations, we work tirelessly to deliver solutions that meet the unique needs of each business we work with.</p>
-            <a href="mailto:krushnamk1009@gmail.com" className="inline-flex items-center text-white bg-orange-700 hover:bg-orange-600 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
+            <a href="mailto:office@cresite.software" className="inline-flex items-center text-white bg-orange-700 hover:bg-orange-600 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
                 Connect With Team
                 <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
