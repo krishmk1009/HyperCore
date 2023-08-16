064f9586434e8e2b47b0df8ff6be2a300c71a0b8
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2023-08-17 01:28:16 +0530

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -26,7 +26,7 @@ const Features = () => {
                                     MERN Stack
                                 </span>
                             </div>
-                            <a href="mailto:krushnamk1009@gmail.com" className="inline-flex items-center font-medium text-orange-600 dark:text-primary-500 hover:underline">
+                            <a href="mailto:office@cresite.software" className="inline-flex items-center font-medium text-orange-600 dark:text-primary-500 hover:underline">
                                 Mail us
                                 <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </a>
@@ -49,7 +49,7 @@ const Features = () => {
                                     Wordpress
                                 </span>
                             </div>
-                            <a href="mailto:krushnamk1009@gmail.com" className="inline-flex items-center font-medium text-orange-600 dark:text-primary-500 hover:underline">
+                            <a href="mailto:office@cresite.software" className="inline-flex items-center font-medium text-orange-600 dark:text-primary-500 hover:underline">
                                 Mail us
                                 <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </a>
